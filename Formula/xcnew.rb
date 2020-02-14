6bfb85aafa821efa7aa2b382bc4de051493d7c30
Ryosuke Ito <rito.0305@gmail.com>
2020-02-15 06:38:17 +0900

    Update xcnew to 0.4.0

--- Formula/xcnew.rb
+++ Formula/xcnew.rb
@@ -1,8 +1,8 @@
 class Xcnew < Formula
   desc 'A command line interface to make a project for iOS Single View App.'
   homepage 'https://github.com/manicmaniac/xcnew'
-  url 'https://github.com/manicmaniac/xcnew/archive/0.3.2.tar.gz'
-  sha256 '91911d20252cd95b8445961c7c8ae9098c096c7e734fd3cac04d8ceff1b50f3f'
+  url 'https://github.com/manicmaniac/xcnew/archive/0.4.0.tar.gz'
+  sha256 'd8596a659c429d5d4d669480bd2b1f7aae02ed6881b9877192e97353bf9c6669'
 
   depends_on :xcode
 
