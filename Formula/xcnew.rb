dc0cdef62aa1185920946fe4526767986308d3b7
Ryosuke Ito <rito.0305@gmail.com>
2021-04-06 13:59:35 +0900

    Update xcnew to 0.5.3

--- Formula/xcnew.rb
+++ Formula/xcnew.rb
@@ -1,8 +1,8 @@
 class Xcnew < Formula
   desc "Missing command-line interface to make a project for iOS Single View App"
   homepage "https://github.com/manicmaniac/xcnew"
-  url "https://github.com/manicmaniac/xcnew/archive/0.5.2.tar.gz"
-  sha256 "d21f8e537a61a9be0e5c9230987efdb972fdaf2892ecf805722fbf489c2f3fd5"
+  url "https://github.com/manicmaniac/xcnew/archive/0.5.3.tar.gz"
+  sha256 "9707c5715ef599c022b90f1ba2e310db44085f2fec55fdf336d2b7e250bea2ed"
 
   depends_on :xcode
 
