209991dc965fa27989af4a19b668042cbf9ab25c
Ryosuke Ito <rito.0305@gmail.com>
2022-07-29 00:01:46 +0900

    Description should not end with full stop

--- Formula/class-dump.rb
+++ Formula/class-dump.rb
@@ -1,5 +1,5 @@
 class ClassDump < Formula
-  desc "Generate Objective-C headers from Mach-O files (forked from nygard/class-dump)."
+  desc "Generate Objective-C headers from Mach-O files (forked from nygard/class-dump)"
   homepage "https://github.com/manicmaniac/class-dump"
   url "https://github.com/manicmaniac/class-dump/archive/3.5-0.1.0.tar.gz"
   sha256 "e17c98776c01ab397b8b1b2c56062e27ef96a4d59f314bdda840c07490ca68be"
