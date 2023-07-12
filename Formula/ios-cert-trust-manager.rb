112006242dc44f3ee5cc3c39a3e98b57162e639a
Ryosuke Ito <rito.0305@gmail.com>
2023-07-12 10:39:34 +0900

    Shorten description and put version before sha256

--- Formula/ios-cert-trust-manager.rb
+++ Formula/ios-cert-trust-manager.rb
@@ -1,9 +1,9 @@
 class IosCertTrustManager < Formula
-  desc "Simple management script to import/list/remove CA certificates to the iOS simulator."
+  desc "Simple management script to import/list/remove CA certificates to iOS simulator"
   homepage "https://github.com/manicmaniac/ADVTrustStore"
   url "https://raw.githubusercontent.com/manicmaniac/ADVTrustStore/84f06050c11666fed4a24c378003ca3d70b8d26c/iosCertTrustManager.py"
-  sha256 "ee709cbd99375490707c366ee5c5b83e9aa05732b964c7e07bb2a7ce395dfe51"
   version "1.0.0"
+  sha256 "ee709cbd99375490707c366ee5c5b83e9aa05732b964c7e07bb2a7ce395dfe51"
 
   def install
     bin.install buildpath/"iosCertTrustManager.py"
