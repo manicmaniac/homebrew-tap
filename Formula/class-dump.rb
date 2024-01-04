bd41c730a05ea6e80269df3bed591160366ffb81
Ryosuke Ito <rito.0305@gmail.com>
2024-01-04 10:10:15 +0900

    Fix URL of class-dump and xcnew
    To be compliant with the linter error.
    > Error: 1 problem in 1 formula detected.
    >   * line 4, col 3: Use refs/tags/3.5-0.2.1 or refs/heads/3.5-0.2.1 for
    >     GitHub references (url is
    > https://github.com/manicmaniac/class-dump/archive/3.5-0.2.1.tar.gz).

--- Formula/class-dump.rb
+++ Formula/class-dump.rb
@@ -1,7 +1,7 @@
 class ClassDump < Formula
   desc "Generate Objective-C headers from Mach-O files (forked from nygard/class-dump)"
   homepage "https://github.com/manicmaniac/class-dump"
-  url "https://github.com/manicmaniac/class-dump/archive/3.5-0.2.1.tar.gz"
+  url "https://github.com/manicmaniac/class-dump/archive/refs/tags/3.5-0.2.1.tar.gz"
   sha256 "4a9d1fee53fbd5b5da02e8f949862d9623be73c0d7c4f652951674c5fd2cd741"
 
   depends_on :xcode
--- Formula/xcnew.rb
+++ Formula/xcnew.rb
@@ -1,7 +1,7 @@
 class Xcnew < Formula
   desc "Missing command-line interface to make a project for iOS Single View App"
   homepage "https://github.com/manicmaniac/xcnew"
-  url "https://github.com/manicmaniac/xcnew/archive/0.6.6.tar.gz"
+  url "https://github.com/manicmaniac/xcnew/archive/refs/tags/0.6.6.tar.gz"
   sha256 "ebd0cfbfc723188b6b26dc20c63152d415cc6ee47f839025b0ba1b2f0592dad1"
 
   depends_on :xcode
